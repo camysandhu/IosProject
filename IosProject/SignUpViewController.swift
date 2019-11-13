cd95fbc228663268a3844feb7d3c4f9b7ade3fe8
camysandhu <fmsharmn@gmail.com>
2019-11-13 15:59:22 -0500

    welcome view design

---
+++ IosProject/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  IosProject
+//
+
+//  Copyright © 2019 Camy. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+  
+
+}

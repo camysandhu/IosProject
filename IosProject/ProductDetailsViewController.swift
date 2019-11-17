0d48534a0cf0777c6c7c52164beda0348e635527
varinderdhillon123 <varinder701dhillon@gmail.com>
2019-11-17 18:27:41 -0500

    [varinder] product details class created with buttons

---
+++ IosProject/ProductDetailsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProductDetailsViewController.swift
+//  IosProject
+//
+//  Created by komaldeep kaur on 2019-11-15.
+//  Copyright © 2019 Camy. All rights reserved.
+//
+
+import UIKit
+
+class ProductDetailsViewController: UIViewController {
+     
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var brandLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    @IBOutlet weak var descLbl: UILabel!
+    @IBOutlet weak var quantityTxt: UITextField!
+    override func viewDidLoad() {
+        
+
+        
+    }
+    
+
+}

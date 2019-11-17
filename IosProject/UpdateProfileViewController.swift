d39293ca4b019e3855f27de5269a1bb874e8a232
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-11-17 17:01:49 -0500

    [charan] perform segue for update profile

--- IosProject/UpdateProfileViewController.swift
+++ IosProject/UpdateProfileViewController.swift
@@ -4,7 +4,10 @@ import UIKit
 
 class UpdateProfileViewController: UIViewController {
 
-
+    @IBOutlet weak var udUserTxt: UITextField!
+       @IBOutlet weak var upPassTxt: UITextField!
+       @IBOutlet weak var upEmailTxt: UITextField!
+       @IBOutlet weak var upAddressTxt: UITextField!
 
 
 }

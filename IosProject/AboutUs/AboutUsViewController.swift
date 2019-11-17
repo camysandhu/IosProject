3f75ba33dd594658678640489d869d5f5162cc53
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-11-17 14:34:50 -0500

    [charan] about us html file and view controller file-> complete

---
+++ IosProject/AboutUs/AboutUsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AboutUsViewController.swift
+//  IosProject
+//
+//  Created by charanpreet kaur on 2019-11-16.
+//  Copyright © 2019 Camy. All rights reserved.
+//
+
+import UIKit
+import WebKit
+//https://stackoverflow.com/questions/49638653/load-local-web-files-resources-in-wkwebview
+class AboutUsViewController: UIViewController, WKNavigationDelegate {
+
+    
+
+    @IBOutlet weak var aboutUsPage: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadHTMLFile()
+        
+    }
+    
+    func loadHTMLFile(){
+        let filepath = Bundle.main.url(forResource: "AboutUsData", withExtension: "html")
+        let aboutUsLink = URLRequest(url: filepath!)
+        aboutUsPage.load(aboutUsLink)
+    }
+
+}

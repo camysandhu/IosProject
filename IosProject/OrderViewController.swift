7037586f6b06631bd30a1494791ef125f48ef09e
charanpreetkaur026 <varinder701dhillon@gmail.com>
2019-11-17 19:37:52 -0500

    [charan] OrderViewController created

---
+++ IosProject/OrderViewController.swift
@@ -0,0 +1,28 @@
+
+
+import UIKit
+
+class OrderViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+         
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return
+    }
+    
+    
+    
+
+    
+
+}

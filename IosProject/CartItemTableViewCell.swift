8cd1f734b226ab63a04581b402499137311d1aca
charanpreetkaur026 <varinder701dhillon@gmail.com>
2019-11-17 19:16:36 -0500

    [charan] CartTableViewCell

---
+++ IosProject/CartItemTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CartItemTableViewCell.swift
+//  IosProject
+//
+//  Created by komaldeep kaur on 2019-11-16.
+//  Copyright © 2019 Camy. All rights reserved.
+//
+
+import UIKit
+
+class CartItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}

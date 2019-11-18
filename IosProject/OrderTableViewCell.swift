21038274ff595585ecf3ff1e4f6aa3667c713d7f
charanpreetkaur026 <varinder701dhillon@gmail.com>
2019-11-17 19:53:47 -0500

    [charan] created outlets for order table cell

--- IosProject/OrderTableViewCell.swift
+++ IosProject/OrderTableViewCell.swift
@@ -5,6 +5,9 @@ import UIKit
 class OrderTableViewCell: UITableViewCell {
 
     
+    @IBOutlet weak var orderMethodLbl: UILabel!
+    @IBOutlet weak var orderShippLbl: UILabel!
+    @IBOutlet weak var orderTotalAmountLbl: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code

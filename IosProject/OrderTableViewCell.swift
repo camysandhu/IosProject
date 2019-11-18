5c4ef62fd1e54fb7b531ebac965c8c0d78a097a1
charanpreetkaur026 <varinder701dhillon@gmail.com>
2019-11-17 19:54:29 -0500

    [charan] set selected function

--- IosProject/OrderTableViewCell.swift
+++ IosProject/OrderTableViewCell.swift
@@ -12,6 +12,10 @@ class OrderTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
 
+        // Configure the view for the selected state
+    }
    
 }

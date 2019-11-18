944a6016301e7427152023dddfb9b3e6f36522ba
charanpreetkaur026 <varinder701dhillon@gmail.com>
2019-11-17 19:21:15 -0500

    [charan] PaymentViewController created

---
+++ IosProject/PaymentViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PaymentViewController.swift
+//  IosProject
+//
+//  Created by komaldeep kaur on 2019-11-16.
+//  Copyright © 2019 Camy. All rights reserved.
+//
+
+import UIKit
+
+class PaymentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+    }
+}
+   
+   
+                    
+               
+    
+    
+    
+
